8be08a9dfbee9413b5d0f8d15353c310bddd1736
Bartłomiej Filipek <bartlomiej.filipek@gmail.com>
2014-09-15 17:23:41 +0200

    cpu tools test exe files

---
+++ cpu_tools_test/run_all.bat
@@ -0,0 +1,4 @@
+cpuTest_default.exe > times_default.txt
+cpuTest_fp_fast.exe > times_fp_fast.txt
+cpuTest_fp_fast_avx.exe > times_fp_fast_avx.txt
+cpuTest_fp_fast_avx_03.exe > times_fp_fast_avx_03.txt
