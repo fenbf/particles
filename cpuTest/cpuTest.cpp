0b1eecb610ddacd132bd9c2d448548b257ccfdba
Bartłomiej Filipek <bartlomiej.filipek@gmail.com>
2014-09-15 17:20:13 +0200

    default member initialization fix for VS2013, timer query in the benchmark

--- cpuTest/cpuTest.cpp
+++ cpuTest/cpuTest.cpp
@@ -7,7 +7,34 @@
 #include "particles.h"
 #include "effect.h"
 
+#include <chrono>
+
 using namespace particles;
+
+class CpuTimeQuery
+{
+protected:
+	double m_time; 
+
+protected:
+	std::chrono::time_point<std::chrono::high_resolution_clock> m_cpuTimePointStart;
+
+public:
+	void begin()
+	{
+		m_cpuTimePointStart = std::chrono::high_resolution_clock::now();
+	}
+
+	void end()
+	{
+		auto diff = std::chrono::high_resolution_clock::now() - m_cpuTimePointStart;
+		auto mili = std::chrono::duration_cast<std::chrono::milliseconds>(diff).count();
+		m_time = 0.5*(m_time + (double)mili);
+	}
+
+	double timeInMilisec() const { return m_time;  }
+	double timeInSeconds() const { return m_time*0.001; }
+};
 
 int main()
 {
@@ -20,38 +47,46 @@ int main()
 	const double DELTA_TIME{ 1.0 / 60.0 };	// 60 fps
 	const size_t FRAME_COUNT{ 200 };
 
+	CpuTimeQuery timer;
+
 	std::cout << "count\t";
 	for (const auto &n : EFFECTS_NAME)
-		std::cout << n.c_str() << "\t";
+		std::cout << n.c_str() << "|\t";
 	std::cout << std::endl;
+	std::cout << "-------|----------" << std::endl;
+
+	std::cout.setf(std::ios::fixed, std::ios::floatfield);
+	std::cout.precision(3);
+	std::wcout << std::fixed;
 
 	for (size_t step = 0; step < PARTICLES_NUM_STEPS; ++step)
 	{
 		size_t numParticles{ START_NUM_PARTICLES + step*NUM_PARTICLES_STEP };
-		std::cout << numParticles << "\t";
+		
+		if (numParticles < 150000 || numParticles > 200000)
+			continue;
+		
+		std::cout << numParticles << " | ";		
 
 		for (const auto &n : EFFECTS_NAME)
 		{
 			auto e = EffectFactory::create(n.c_str());
 			e->initialize(numParticles);
 
-			LARGE_INTEGER freq, startTime, endTime, elapsedMicroseconds;
-			QueryPerformanceFrequency(&freq);
-			QueryPerformanceCounter(&startTime);
+			timer.begin();
 
 			for (size_t frame = 0; frame < FRAME_COUNT; ++frame)
 			{
 				e->cpuUpdate(DELTA_TIME);
 			}
-			QueryPerformanceCounter(&endTime);
-			elapsedMicroseconds.QuadPart = endTime.QuadPart - startTime.QuadPart;
-			elapsedMicroseconds.QuadPart *= 100000;
-			elapsedMicroseconds.QuadPart /= freq.QuadPart;
+			timer.end();
 
-			std::cout << elapsedMicroseconds.QuadPart << "\t";
+			std::cout << timer.timeInMilisec() << " | ";
 		}
 		std::cout << std::endl;
 	}	
+
+	std::cout << "time in miliseconds" << std::endl;
 
 	return 0;
 }
